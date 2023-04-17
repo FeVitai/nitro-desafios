40e51a8268a219c3dffd8675da858f753ff8049b
Felipe Vitai Afanasiev <felipevitai93@gmail.com>
2023-04-17 04:03:20 -0300

    desafios

---
+++ src/desafios/solucoes/ChegaDeMultas.java
@@ -0,0 +1,37 @@
+package desafios.solucoes;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ChegaDeMultas {
+
+	/*
+	 * Chega de multas Mostrar detalhes Carlos viajou para outro país e alugou um
+	 * carro, no dia da devolução foi notificado que havia 300 reais em multa por
+	 * alta velocidade. Após longas conversas percebeu que o velocímetro estava
+	 * configurado em milhas por horas, então enquanto achava que estava a 80Km/h na
+	 * verdade estava em 80mph. Lembrando que 1 km/h corresponde a 0,62137 mph.
+	 * 
+	 * Tarefa Escreva um programa que converta o valor em milhas/h para Km/h.
+	 * 
+	 * Entrada A entrada cons1.6093440006147iste de um único número N.
+	 * 
+	 * Saída Seu programa deve imprimir uma única linha, contendo o valor convertido
+	 * para Km/h (com duas casas decimais).
+	 */
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		double mile = 1.6093440006147;
+		
+		double n = sc.nextDouble();
+		
+		System.out.printf("%.2f", n * mile);
+		
+		sc.close();
+	}
+	
+}
---
+++ src/desafios/solucoes/Espelho.java
@@ -0,0 +1,37 @@
+package desafios.solucoes;
+
+import java.util.Scanner;
+
+public class Espelho {
+
+	/*
+	 * Espelho Mostrar detalhes Por meio de um espelho plano de maneira metafórica e
+	 * matematicamente falando pode - se espelhar números, com isso, espelhando um
+	 * número de uma reta numérica irá se obter o seu número oposto. Gustavo deseja
+	 * exemplificar essa técnica utilizando o conjunto de números inteiros. Ajude-o
+	 * nesta tarefa
+	 * 
+	 * Tarefa Escreva um programa que, dado um número inteiro exiba o seu oposto no
+	 * conjunto de números inteiros.
+	 * 
+	 * Entrada Consiste de um inteiro N, onde N != 0. O programa deve executar até
+	 * que receba como entrada o número 0.
+	 * 
+	 * Saída Para cada N seu programa deve imprimir o oposto de N no conjunto de
+	 * números inteiros.
+	 */
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		while (n != 0) {
+			System.out.println(n * -1);
+			n = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+}
