04a0cfd0d99040edc6a7a47b9455712ad32bf663
Felipe Vitai Afanasiev <felipevitai93@gmail.com>
2023-05-08 22:53:20 -0300

    a

---
+++ src/desafios/solucoes/InverterSequencia.java
@@ -0,0 +1,37 @@
+package desafios.solucoes;
+
+import java.util.Scanner;
+
+public class InverterSequencia {
+
+	/*
+	 * Nathan estava escrevendo uma sequência de matriculas para cadastro na
+	 * disciplina de programação, após finalizar a atividade ele percebeu que havia
+	 * ordenado de maneira incorreta pois o primeiro da sequencia deveria ser o
+	 * ultimo, o segundo o penúltimo e assim por diante. Para não precisar refazer o
+	 * processo de separação escreva um programa que inverta uma sequencia.
+	 * 
+	 * Entrada A primeira linha da entrada contém um inteiro N que indica o número
+	 * de matrículas (1 ≤ N ≤ 40). A segunda linha contém a sequência de
+	 * N-matrículas na sequência incorreta.
+	 * 
+	 * Saída Seu programa deve imprimir uma única linha, contendo a sequência
+	 * correta.
+	 */	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] matriculas = new int [n];
+		
+		for(int i=0; i<matriculas; i++) {
+			
+		}
+		
+		
+		
+		sc.close();
+	}
+}
